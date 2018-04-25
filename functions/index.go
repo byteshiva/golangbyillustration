410ae33694f2004710404d560cd790334d9ff88d
siva <byteshiva@gmail.com>
2018-04-25 08:41:33 +0530

    fix code format

--- functions/index.go
+++ functions/index.go
@@ -3,14 +3,14 @@ package main
 import "fmt"
 
 func plus(a int, b int) int {
-	return a + b;
+	return a + b
 }
 
-func plusplus(a , b, c int) int {
-	return a + b + c;
+func plusplus(a, b, c int) int {
+	return a + b + c
 }
 
-func main()  {
+func main() {
 	res := plus(1, 3)
 	fmt.Println("1+2 =", res)
 
