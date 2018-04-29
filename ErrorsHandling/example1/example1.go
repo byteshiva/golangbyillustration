e1cdbd645d8cfb5c9c35442814549be26fe5a46d
siva <byteshiva@gmail.com>
2018-04-29 09:20:33 +0530

    1. Example to show how the error type is implemented

---
+++ ErrorsHandling/example1/example1.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+// http://golang.org/pkg/builtin#error
+type error interface {
+	Error() string
+}
+
+// http://golang.org/pkg/errors/error.go
+type errorString struct {
+	s string
+}
+
+// http://golang.org/pkg/errors/errors.go
+func (e *errorString) Error() string {
+	return e.s
+}
+
+// http://golang.org/src/pkg/errors/errors.go
+// New returns an error that formats as the given text.
+func New(text string) error {
+	return &errorString{text}
+}
+
+func main() {
+	if err := randomAPICall(); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Life is good... cool")
+}
+
+func randomAPICall() error {
+	return New("Bad Request")
+}
