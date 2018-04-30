546bf87386db77edc637eea0424ea7c212ba5960
siva <byteshiva@gmail.com>
2018-04-30 10:06:57 +0530

    Example shows how to reflect over a struct type value that is stored.

---
+++ interfaces/example1/example1.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type user struct {
+	name     string
+	age      int
+	building float32
+	secure   bool
+	roles    []string
+}
+
+func main() {
+	// Create a struct type user value.
+	u := user{
+		name:     "Cindy",
+		age:      27,
+		building: 321.44,
+		secure:   true,
+		roles:    []string{"admin", "developer", "customer"},
+	}
+
+	// Store a copy of the user value inside an empty interface value.
+	var i interface{} = u
+
+	// Display information about the user value that was stored.
+	v := reflect.ValueOf(i)
+	fmt.Printf("Kind: %v\t Type: %v\t\t NumFileds: %v\n", v.Kind, v.Type, v.NumField())
+
+}
